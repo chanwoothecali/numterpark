f5f17d6df6a61ddd33f8f5a87ab80f85be5b0201
jungcali94 <jungcali94@gmail.com>
2023-06-16 09:35:44 +0900

    first commit

---
+++ src/main/java/com/numble/numterpark/NumterparkApplication.java
@@ -0,0 +1,13 @@
+package com.numble.numterpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumterparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumterparkApplication.class, args);
+	}
+
+}
