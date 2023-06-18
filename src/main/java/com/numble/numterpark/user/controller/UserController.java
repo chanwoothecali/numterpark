2837ed463e8f9afc39846173f7fa5c1c2e5c6546
macCali <jungcali94@gmail.com>
2023-06-18 17:04:05 +0900

    회원가입 컨트롤러 생성

---
+++ src/main/java/com/numble/numterpark/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.numble.numterpark.user.controller;
+
+import com.numble.numterpark.user.service.UserService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@RequestBody @Valid UserDto.CreateRequest createRequest) {
+        userService.createUser(createRequest);
+    }
+}
