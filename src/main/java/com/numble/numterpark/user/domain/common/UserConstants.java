6c3405f83a7e5666766352b3b6a89c3faf2c4d09
chanwoothecali <jungcali94@gmail.com>
2023-06-19 15:07:45 +0900

    UserService login 로직 추가
    * UserService login 로직 추가
      - Service 로직 생성
      - 테스트코드 작성
      - 관련 예외 및 상수 생성

---
+++ src/main/java/com/numble/numterpark/user/domain/common/UserConstants.java
@@ -0,0 +1,6 @@
+package com.numble.numterpark.user.domain.common;
+
+public class UserConstants {
+
+    public static final String EMAIL = "email";
+}
--- src/test/java/com/numble/numterpark/user/service/UserServiceTest.java
+++ src/test/java/com/numble/numterpark/user/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.numble.numterpark.user.service;
 
+import static com.numble.numterpark.user.domain.common.UserConstants.EMAIL;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -7,20 +8,27 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
 import com.numble.numterpark.user.controller.UserDto;
+import com.numble.numterpark.user.controller.UserDto.LoginRequest;
 import com.numble.numterpark.user.domain.common.UserRole;
 import com.numble.numterpark.user.domain.entity.User;
 import com.numble.numterpark.user.encoder.PasswordEncoder;
 import com.numble.numterpark.user.exception.DuplicatedEmailException;
+import com.numble.numterpark.user.exception.NotExistedUserException;
 import com.numble.numterpark.user.repository.UserRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpSession;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
 
     @Mock
     private UserRepository userRepository;
@@ -28,12 +36,13 @@ class UserServiceTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    @InjectMocks
-    private UserService userService;
+    @Mock
+    private MockHttpSession session;
 
-    private final String EXISTING_EMAIL = "test@modu.com";
 
-    private final String CORRECT_PASSWORD = "test12345@";
+    private final String USER_EMAIL = "test@modu.com";
+
+    private final String USER_PASSWORD = "test12345@";
 
     @Test
     @DisplayName("정상적으로 회원가입에 성공한다.")
@@ -55,20 +64,52 @@ class UserServiceTest {
     public void emailDuplicated_createUser_failure() {
         // given
         UserDto.CreateRequest createRequest = createTestUserData();
-        given(userRepository.existsByEmail(EXISTING_EMAIL)).willReturn(true);
+        given(userRepository.existsByEmail(USER_EMAIL)).willReturn(true);
 
         // when
         assertThrows(DuplicatedEmailException.class, () -> userService.createUser(createRequest));
 
         // then
-        then(userRepository).should().existsByEmail(EXISTING_EMAIL);
+        then(userRepository).should().existsByEmail(USER_EMAIL);
         then(userRepository).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    @DisplayName("로그인 - 이메일과 패스워드를 잘못 입력해 로그인에 실패한다..")
+    public void notExistUser() throws Exception {
+        // given
+        LoginRequest loginRequest = loginRequest();
+        given(userRepository.findByEmailAndPassword(loginRequest.getEmail(),
+            passwordEncoder.encrypt(loginRequest().getPassword()))).willReturn(Optional.empty());
+
+        // when
+        assertThrows(NotExistedUserException.class, () -> userService.login(loginRequest));
+
+        // then
+        then(userRepository).should().findByEmailAndPassword(loginRequest.getEmail(),
+            passwordEncoder.encrypt(loginRequest().getPassword()));
+        then(session).shouldHaveNoInteractions();
+    }
+
+    @Test
+    @DisplayName("로그인 - 이메일, 패스워드가 일치하는 사용자는 로그인에 성공한다.")
+    public void login_successful() throws Exception {
+        // given
+        LoginRequest loginRequest = loginRequest();
+        given(userRepository.findByEmailAndPassword(USER_EMAIL, passwordEncoder.encrypt(USER_PASSWORD))).willReturn(Optional.of(createUser()));
+
+        // when
+        userService.login(loginRequest);
+
+        // then
+        then(userRepository).should().findByEmailAndPassword(USER_EMAIL, passwordEncoder.encrypt(USER_PASSWORD));
+        then(session).should().setAttribute(EMAIL, USER_EMAIL);
     }
 
     private UserDto.CreateRequest createTestUserData() {
         return UserDto.CreateRequest.builder()
-            .email(EXISTING_EMAIL)
-            .password(CORRECT_PASSWORD)
+            .email(USER_EMAIL)
+            .password(USER_PASSWORD)
             .name("테스트네임")
             .phoneNumber("01012345678")
             .build();
@@ -76,11 +117,18 @@ class UserServiceTest {
 
     private User createUser() {
         return User.builder()
-            .email(EXISTING_EMAIL)
+            .email(USER_EMAIL)
             .name("테스트네임")
-            .password(CORRECT_PASSWORD)
+            .password(USER_PASSWORD)
             .role(UserRole.BUYER)
             .phoneNumber("01012345678")
+            .build();
+    }
+
+    private UserDto.LoginRequest loginRequest() {
+        return LoginRequest.builder()
+            .email(USER_EMAIL)
+            .password(USER_PASSWORD)
             .build();
     }
 }
